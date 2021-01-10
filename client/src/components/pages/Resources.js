a0e79a60a6c06bcb93b0d71d09c4347414989fc3
sara-ngo <sara.ngo.9641@gmail.com>
2021-01-09 20:23:52 -0800

    some fix for mobile-friendly interactions

--- client/src/components/pages/Resources.js
+++ client/src/components/pages/Resources.js
@@ -4,6 +4,7 @@ import Info from '../resources/Info';
 import Footer from '../footer/Footer';
 
 function Resources() {
+  document.documentElement.scrollTop = 0; 
   return (
     <>
       <Info />
