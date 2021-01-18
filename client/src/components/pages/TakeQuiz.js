eb515d04085f94c9104313c598108eda95d14801
Hang Ho <bemoon0103@gmail.com>
2021-01-17 19:34:07 -0800

    fixed start btn

--- client/src/components/pages/TakeQuiz.js
+++ client/src/components/pages/TakeQuiz.js
@@ -15,13 +15,13 @@ function TakeQuiz() {
           <h2>This quiz has 20 multiple choice questions which determines the political party that best suits your interests.
           There is no time limited but please finish the quiz at one sitting since you can't resume your quiz after you quit. 
           Give careful consideration to your answers because you can't go back to the previous question to change your answer. 
-          When you ready, click on "Let Start" button below to start your quiz.</h2>
+          When you ready, click on "Start" button below to start your quiz.</h2>
         </div>
         
         <div className="text-center">
           <Link to="/Quiz">
             <Button buttonStyle='btn--form' buttonSize='btn-medium'>
-              Let Start
+              Start
             </Button>
           </Link>
           <GoBackBtn />
